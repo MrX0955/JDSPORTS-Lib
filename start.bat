f4923e83a41d9b3c5512d9f2dd5551b79198351f
Empty <77683874+MrX0955@users.noreply.github.com>
2023-02-14 05:54:40 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,27 @@
+pip install requests mohawk
+color 0a
+@echo off
+cls
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+timeout 5
+start https://cracked.io/Sweety666
