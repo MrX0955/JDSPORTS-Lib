ddaa3c611be8653272aa7c9e3546106e04b3c514
Empty <77683874+MrX0955@users.noreply.github.com>
2023-02-14 05:59:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #This is how u'll use the library in your own project. In addition, u can also use it just for capture.
 
+from JDSPORTS import solver
+
 solver(username="EmailHere", password="PasswordHere")
